912138880c8b8836fe6d3faeaee228ab9ae31509
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2022-07-03 11:30:51 +0200

    Add function for checking opponents' choices in a single round

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ function computerPlay() {
     return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    return "It's a tie";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose. ${computerSelection} beats ${playerSelection}`;
+  }
+}
