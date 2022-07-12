987e235d4ab498165ad331160bd6648ed81f7c30
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2022-07-12 12:50:58 +0200

    Link game and playRound functions

--- app.js
+++ app.js
@@ -13,48 +13,53 @@ function playRound(e) {
   playerSelection = e.target.className;
   computerSelection = computerPlay();
   scoreMessage = document.querySelector("p");
-  if (playerSelection === computerSelection) {
-    scoreMessage.textContent = "It's a tie";
-    return 0;
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    scoreMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-    return 1;
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    scoreMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-    return 1;
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    scoreMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-    return 1;
+  scoreCounter = document.querySelector(".score-counter");
+  if (scores[0] == 5 || scores[1] == 5) {
+    if (scores[0] === 5) {
+      scoreMessage.textContent = "Human Wins";
+    } else {
+      scoreMessage.textContent = "Computer Wins";
+    }
   } else {
-    scoreMessage.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-    return 2;
+    // checks which side has won the round
+    if (playerSelection === computerSelection) {
+      scoreMessage.textContent = "It's a tie";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+      scoreMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+      scores[0] = scores[0] + 1;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+      scoreMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+      scores[0] = scores[0] + 1;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+      scoreMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+      scores[0] = scores[0] + 1;
+    } else {
+      scoreMessage.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+      scores[1] = scores[0] + 1;
+    }
+    // adjusts score counter
+    scoreCounter.textContent = `${scores[0]}  :  ${scores[1]}`;
+    if (scores[0] == 5 || scores[1] == 5) {
+      if (scores[0] === 5) {
+        scoreMessage.textContent = "Human Wins";
+      } else {
+        scoreMessage.textContent = "Computer Wins";
+      }
+    }
   }
 }
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let winnerIndicator = -1;
-  let playerChoice = "";
-  winnerIndicator = playRound(playerChoice, computerPlay());
-  if (winnerIndicator == 2) {
-    computerScore += 1;
-  } else if (winnerIndicator == 1) {
-    playerScore += 1;
-  }
-  if (playerScore > computerScore) {
-    console.log("You win the round");
-  } else if (computerScore > playerScore) {
-    console.log("You lose the round");
-  } else {
-    console.log("This round is a tie");
-  }
+  // event listener for buttons for player choice
+  const rockButton = document.querySelector(".rock");
+  const paperButton = document.querySelector(".paper");
+  const scissorsButton = document.querySelector(".scissors");
+  rockButton.addEventListener("click", playRound);
+  paperButton.addEventListener("click", playRound);
+  scissorsButton.addEventListener("click", playRound);
 }
 
-const rockButton = document.querySelector(".rock");
-const paperButton = document.querySelector(".paper");
-const scissorsButton = document.querySelector(".scissors");
+// array holds score for player and computer index 0 and index 1 is computer
+const scores = [0, 0];
 
-rockButton.addEventListener("click", playRound);
-paperButton.addEventListener("click", playRound);
-scissorsButton.addEventListener("click", playRound);
+game();
